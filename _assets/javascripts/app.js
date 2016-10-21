15ea4fe4242ec9221b2d8c94b939a1bbb79deab8
Martin Laws <martin.laws@indexexchange.com>
2016-10-21 03:02:04 -0400

    making headway with layout

---
+++ _assets/javascripts/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("li").countdown("2016/11/18", function(event) {
+    $('li.days span').html(
+      event.strftime('%D') + '<span> Days</span>'
+    );
+    $('li.hours span').html(
+      event.strftime('%H') + '<span> Hours</span>'
+    );
+    $('li.minutes span').html(
+      event.strftime('%M') + '<span> Minutes</span>'
+    );
+    $('li.seconds span').html(
+      event.strftime('%S') + '<span> Seconds</span>'
+    );
+  });
+});
