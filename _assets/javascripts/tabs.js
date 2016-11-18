1b7e9ca3d66c4d05fbcd92624d03a07996fd600d
Martin Laws <martin.laws@indexexchange.com>
2016-11-18 11:20:01 -0500

    Added schedule

--- _assets/javascripts/tabs.js
+++ _assets/javascripts/tabs.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
 
-	$('ul.tabs li').click(function(){
+	$('ul.tabs.faq li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
+		$('ul.tabs.faq li').removeClass('current');
+		$('.faq-tab-content').removeClass('current');
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
     console.log(this);
+	})
+
+	$('ul.tabs.sched li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs.sched li').removeClass('current');
+		$('.sched-tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+		console.log(this);
 	})
 
 })
